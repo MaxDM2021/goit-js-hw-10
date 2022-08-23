2053e65f753f9527818bd262a2d880b7b37f1c96
Maksym Dmitriiev <maximdm22@gmail.com>
2022-08-23 16:46:43 +0300

    111

---
+++ src/fetchContries.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://restcountries.com/v2';
+
+
+function fetchContries (name) {
+
+  return fetch(`${BASE_URL}/name/${name}`).then( response => response.json());
+}
+
+
+
+export default { fetchContries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import './css/styles.css';
+import handelbars from 'express-handlebars';
+
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+import debounce from 'lodash.debounce';
+import countryCardTpl from '../templates/country-card.hbs';
+import getRefs from './get-refs';
+
+import API from './fetchContries'
+
+const refs = getRefs();
 
 const DEBOUNCE_DELAY = 300;
+
+
+  refs.searchForm.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
+
+  function onSearch (e) {
+    e.preventDefault();
+  
+    const form = e.currentTarget;
+    const searchQuery = form.elements.query.value;
+  
+  
+  API.fetchContries(searchQuery)
+  .then(renderCountryCard)
+  .catch(onFetchError)
+  .finally(() => form.reset());
+  }
+
+
+  function renderCountryCard(country) {
+    const markup = countryCardTpl(country);
+    refs.countryInfoContainer.innerHTML = markup;
+  }
+  
+  function onFetchError (error) {
+    Notiflix.Notify.failure('❌ Oops, there is no country with that name', {
+      timeout: 2000,
+    });
+  }
